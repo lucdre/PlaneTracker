e52f50ec88a1fa0dcc977ac7befecff3198e3a0d
Lucdre <lucadanielrex95@gmail.com>
2026-06-20 02:32:47 +0200

    Add project files.

---
+++ PlaneTracker.cpp
@@ -0,0 +1,12 @@
+﻿// PlaneTracker.cpp : Defines the entry point for the application.
+//
+
+#include "PlaneTracker.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
